c771a7abb041ed1d70cd8ad031c3b6aef1bc74b1
Vlad Harkusha <sivari888@gmail.com>
2022-08-03 20:40:40 +0300

    add basic markup for task-board-page

--- src/pages/TaskBoardPage.tsx
+++ src/pages/TaskBoardPage.tsx
@@ -1,9 +1,25 @@
 import { FC } from 'react';
+import '../styles/TaskBoardPage.scss';
 
 export const TaskBoardPage: FC = () => {
   return (
     <div className='task-board-page'>
-      XXPage
+      <div className="board">
+        <div className="board__title">To do</div>
+        <div className="board__item">Go shoping</div>
+      </div>
+
+      <div className="board">
+        <div className="board__title">Check</div>
+        <div className="board__item">Tasks</div>
+      </div>
+
+      <div className="board">
+        <div className="board__title">Done</div>
+        <div className="board__item">Clean teeth</div>
+        <div className="board__item">Get breakfast</div>
+        <div className="board__item">Get dressed</div>
+      </div>
     </div>
   );
 };
